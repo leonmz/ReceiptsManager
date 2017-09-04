0ce12516abb22883a89ad99c9ed9df8898a50380
leon <lswxxyla@gmail.com>
2017-09-03 21:48:49 -0400

    Updated test case

--- src/test/java/api/NetidControllerTest.java
+++ src/test/java/api/NetidControllerTest.java
@@ -2,32 +2,18 @@ package api;
 
 
 
-import org.junit.ClassRule;
+import controllers.*;
 import org.junit.Test;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 
 public class NetidControllerTest {
-    @Path("/netid")
-    public static class NetidController {
-        @GET
-        public String printNetID(){
-            return "al2386";
-        }
-    }
 
-    @Test
-    public void netID() throws IOException {
-        NetidController t = new NetidController();
-        String response = t.printNetID();
-        assertEquals("al2386", response);
+        @Test
+        public void netID() {
+            NetidController t = new NetidController();
+            String response = t.printNetID();
+            assertEquals("al2386", response);
     }
 
 
